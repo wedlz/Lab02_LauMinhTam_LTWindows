073e502bd49addd125c43ffa26d149a68d81e563
wedlz <lauminhtam26@gmail.com>
2023-10-28 15:05:59 +0700

    Add project files.

---
+++ Lab02_LauMinhTam_LTWindows/BTtailop1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_LauMinhTam_LTWindows
+{
+    internal class BTtailop1
+    {
+        static int[] NhapMang(int[] a, int n)
+        {
+            string input = Console.ReadLine();
+            string[] nums;
+            nums = input.Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                //a[i] = Convert.ToInt32(nums[i]);
+                a[i] = int.Parse(nums[i]);
+            }
+            return a;
+        }
+
+        static void XuatMang(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("a[" + i + "]: {0}", a[i]);
+            }
+        }
+
+        static int[][] NhapMang2Chieu(int[][] a, int n, int m)
+        {
+            string[][] nums = new string[n][];
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                nums[i] = input.Split(' ');
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i][j] = Convert.ToInt32(nums[i][j]);
+                }
+            }
+            return a;
+        }
+
+        static void XuatMang2Chieu(int[][] a, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0} ", a[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        static void Mang2Chieu()
+        {
+            int n, m;
+            int[][] a;
+            Console.Write("Moi ban nhap so dong: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Moi ban nhap so dong: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            a = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new int[m];
+            }
+            NhapMang2Chieu(a, n, m);
+            XuatMang2Chieu(a, n, m);
+        }
+
+        static bool ChuoiDoiXung()
+        {
+            string s;
+            s = Console.ReadLine();
+            char[] mangChuoi = s.ToCharArray();
+            for (int i = 0; i < mangChuoi.Length; i++)
+            {
+                if (mangChuoi[i] != mangChuoi[mangChuoi.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool snt(int a)
+        {
+            if (a < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a;
+            Console.Write("Nhập n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            a = new int[n];
+            XuatMang(a, n);
+            Console.Write("Các phần tử lẻ ở vị trí chẵn là: ");
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0 && a[i] % 2 != 0)
+                {
+                    Console.Write("{0} ", a[i]); // các phần tử lẻ ở vị trí chẵn
+                }
+                if (max < a[i]) // tìm max
+                {
+                    max = a[i];
+                }
+                if (min > a[i]) // tìm min
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++) // liệt kê số nguyên tố
+            {
+                if (snt(a[i]))
+                {
+                    Console.Write("{0} ", a[i]);
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++) // tìm phần tử âm
+            {
+                if (a[i] < 0)
+                {
+                    Console.Write("{0} ", a[i]);
+                    break;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Max trong day: {0}", max);
+            Console.WriteLine("Min trong day: {0}", min);
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("Tong cac phan tu trong day = {0}", sum);
+        }
+    }
+}
+
---
+++ Lab02_LauMinhTam_LTWindows/BTtailop2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_LauMinhTam_LTWindows
+{
+    internal class BTtailop2
+    {
+        static void nhapMang(int[][] a, int n, int m)
+        {
+            string input;
+            string[][] nums = new string[n][];
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+                nums[i] = input.Split(' ');
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i][j] = Convert.ToInt32(nums[i][j]);
+                }
+            }
+        }
+
+        static void xuatMang(int[][] a, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0} ", a[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int tongCacPhanTuDuong(int[][] a, int n, int m)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i].Where(t => t > 0).Sum();
+            }
+            return sum;
+        }
+
+        static int tongCacPhanTuTrenDuongCheoChinh(int[][] a, int n, int m)
+        {
+            int sum = 0;
+            if (n != m)
+            {
+                return 0;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i][i];
+            }
+            return sum;
+        }
+
+        static int tongCacPhanTuTrongTamGiacTren(int[][] a, int n, int m)
+        {
+            int sum = 0;
+            if (n != m)
+            {
+                return 0;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < m; j++)
+                {
+                    sum += a[i][j];
+                }
+            }
+            return sum;
+        }
+
+        static bool soChan(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static int tongCacPhanTuChan(int[][] a, int n, int m)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i].Where(t => soChan(t)).Sum();
+            }
+            return sum;
+        }
+
+        static int tongCacPhanTuDongThuI(int[][] a, int n, int m, int i)
+        {
+            int sum = 0;
+            if (i > n - 1)
+            {
+                return 0;
+            }
+            sum += a[i].Sum();
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] a;
+            Console.Write("Nhap so hang: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap so cot: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            a = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new int[m];
+            }
+            nhapMang(a, n, m);
+            xuatMang(a, n, m);
+            Console.WriteLine("Tong cac phan tu duong: {0}", tongCacPhanTuDuong(a, n, m));
+            Console.WriteLine("Tong cac phan tu tren duong cheo chinh: {0}", tongCacPhanTuTrenDuongCheoChinh(a, n, m));
+            Console.WriteLine("Tong cac phan tu trong tam giac tren: {0}", tongCacPhanTuTrongTamGiacTren(a, n, m));
+            Console.WriteLine("Tong cac phan tu chan trong ma tran: {0}", tongCacPhanTuChan(a, n, m));
+            Console.WriteLine("Tong cac phan tu o dong thu i: {0}", tongCacPhanTuDongThuI(a, n, m, 1));
+
+        }
+    }
+}
